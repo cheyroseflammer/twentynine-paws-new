cdd9ab473168887ce4d6737912c25382a14b812b
Chey Rose <cheyroseflammer@gmail.com>
2023-02-21 19:54:49 -0800

    ready to start

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import { Navbar, Hero, Grooming, Footer } from '../src/components';
+
 export default function Home() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <Hero />
+      <Grooming />
+      <Footer />
+    </div>
+  );
 }
